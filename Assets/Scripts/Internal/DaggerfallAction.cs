33dc92af7a73b62cdf5b718c898ae5424af5ce03
Thomas Ricouard <ricouard77@gmail.com>
2017-09-18 20:23:42 +0200

    oops

--- Assets/Scripts/Internal/DaggerfallAction.cs
+++ Assets/Scripts/Internal/DaggerfallAction.cs
@@ -696,7 +696,6 @@ namespace DaggerfallWorkshop
         /// </summary>
         public static void DoorText(GameObject triggerObj, DaggerfallAction thisAction)
         {
-            PlayerInfoLook lol = GameManager.Instance.PlayerInfoLook;
             if (GameManager.Instance.PlayerActivate.CurrentMode == PlayerActivateModes.Info)
             {
                 if (thisAction.Index != 0)
